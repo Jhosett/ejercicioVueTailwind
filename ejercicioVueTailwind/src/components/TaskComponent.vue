5897b9339f8098e1d4f1b48f2d1b9df66ad92c06
Jhosett <darwinjhosettbermudezromero@gmail.com>
2023-11-29 13:22:46 -0500

    Ejercicio Listas con Vue y Tailwind

---
+++ ejercicioVueTailwind/src/components/TaskComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+    tarea: {
+        type: String,
+        default: 'Tarea indefinida'
+    },
+    index: {
+        type: Number
+    }
+})
+
+const emit = defineEmits(['borrarTarea'])
+
+function borrarTarea(index) {
+    //console.log('Borrar tarea',index)
+    emit('borrarTarea', index)
+}
+</script>
+
+<template>
+    <div class="flex justify-between bg-gray-200 rounded px-4 py-1 mt-1">
+      {{ props.tarea }}
+      <span class="hover:bg-red-600 hover:cursor-pointer text-center text-white rounded bg-red-400 px-2"
+        @click="borrarTarea(props.index)"
+      >X</span>
+    </div>
+</template>
